5a3919ccd8ab3182035313ec18058ff9e74da263
Bello Soliu <bellosoliu12@gmail.com>
2023-07-07 21:48:23 +0100

    updated

---
+++ SchoolManagementSystem.Api/Controllers/SchoolFeeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolManagementSystem.Core.DTOs.Requests;
+
+namespace SchoolManagementSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchoolFeeController : ControllerBase
+    {
+        private readonly ISchoolFeeService _schoolFeeService;
+
+        public SchoolFeeController(ISchoolFeeService schoolFeeService)
+        {
+            _schoolFeeService = schoolFeeService;
+        }
+
+
+        [HttpPost("set-schoolFee")]
+        public async Task<IActionResult> SetSchoolFee(ClassFeeDto classFee)
+        {
+            await _schoolFeeService.SetSchoolFee(classFee);
+            return Ok();
+        }
+
+
+        [HttpGet("view-schoolFees")]
+        public IActionResult ViewSchoolFees()
+        {
+            var classFees = _schoolFeeService.ViewSchoolFees();
+            return Ok(classFees);
+        }
+
+
+        [HttpPut("edit-schoolFee/{feeId}")]
+        public async Task<IActionResult> EditSchoolFee(int feeId, ClassFeeDto updatedFee)
+        {
+            var result = await _schoolFeeService.EditSchoolFee(feeId, updatedFee);
+            if (result)
+            {
+                return Ok();
+            }
+            return NotFound();
+        }
+    }
+}
--- SchoolManagementSystem.Core/Entities/SchoolFee.cs
+++ SchoolManagementSystem.Core/Entities/SchoolFee.cs
@@ -2,9 +2,11 @@
 {
     public class SchoolFee : EntityBase
     {
+        public int ClassId { get; set; }
         public Class Class { get; set; }
         public string FeeName { get; set; }
         public decimal FeeAmount { get; set; }
         public decimal TotalFees { get; set; }
+        public DateTime SetDate { get; set; }
     }
 }
